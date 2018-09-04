38c5c9b7c5df456ebe4d757119cc43f3992554ca
kidoda <kdevb0x@gmail.com>
2018-09-04 09:01:01 -0700

    add tcp_test.go

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/kidoda/godemonsaw
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
-	github.com/kidoda/go-demonsaw/router v0.0.0-20180903181346-58af233927e4
+	github.com/kidoda/go-demonsaw/router v0.0.0-20180903200736-8a0f6e781ccf
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.2.2
 	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d
 )
+
+replace github.com/kidoda/go-demonsaw/router => ./router
--- router/tcp.go
+++ router/tcp.go
@@ -1,28 +1,60 @@
 package router
 
 import (
+	"bufio"
 	"log"
 	"net"
 )
 
 type TCPConn struct {
-	Conn net.Conn
-	Addr net.TCPAddr
+	Conn     net.Conn
+	Addr     *net.TCPAddr
+	Listener *net.TCPListener
 }
 
 // NewTCPConn creates a new TCPConn and returns a pointer to it.
 func NewTCPConn(host string) (*TCPConn, error) {
 	tcpconn := &TCPConn{}
-	tcpconn.Addr, err = net.ResolveTCPAddr("tcp", host)
+	addr, err := net.ResolveTCPAddr("tcp", host)
+	tcpconn.Addr = addr
 	if err != nil {
 		log.Printf("Error creating NewTCPConn: %s", err)
 		return nil, err
 	}
-	tcpconn.Conn, err = net.ListenTCP(tcpconn.Addr.Network(), *tcpconn.Addr)
-	if err != nil {
-		log.Print(err)
-		return nil, err
-	}
 	return tcpconn, nil
 
+}
+
+func (c *TCPConn) Listen(readchan chan []byte, writechan chan []byte) {
+serveloop:
+	for {
+		conn, err := c.Listener.Accept()
+		if err != nil {
+			log.Print(err)
+			conn.Close()
+			break serveloop
+		}
+		c.Conn = conn
+		var readr = bufio.NewReader(c.Conn)
+		var readBuff = make([]byte, len(readr.Size()))
+		bufcp, _ := readr.ReadBytes(nil)
+		for i := range bufcp {
+			append(readBuff, i)
+		}
+		readchan <- readBuff
+
+		var writeBuff = bufio.NewWriter(c.Conn)
+		for range writechan {
+			writeBuff.Write(<-writechan)
+		}
+	}
+}
+
+func (c *TCPConn) SetListener() error {
+	c.Listener, err = net.ListenTCP(c.Addr.Network(), *c.Addr)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	return nil
 }
